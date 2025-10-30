c139c02c33088757233a4da6a3bbc4520603013d
roshan-shete <roshan.shete@gmail.com>
2025-10-30 18:34:49 +0530

    adding one class

---
+++ src/Gittest1/test_git2.java
@@ -0,0 +1,21 @@
+package Gittest1;
+
+public class test_git2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abc");
+		System.out.println("123");
+		System.out.println("xyz");
+		System.out.println("1111");
+		System.out.println("22222");
+		System.out.println("333333333333");//chbbcbcm
+		System.out.println("ssssssssssssss");
+		System.out.println("44444444444444");
+		System.out.println("8888888888888");
+		System.out.println("dddddddddddddd");		
+		System.out.println("fffffffffffffff");
+		System.out.println("bbbbbbbbbbbbbb");
+	}
+
+}
