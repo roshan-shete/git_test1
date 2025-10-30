a1cbb65a9ca40fea3527254ec2d5493eb975b62f
roshan-shete <roshan.shete@gmail.com>
2025-10-30 12:47:26 +0530

    second commit

---
+++ src/Gittest1/for_learn_git.java
@@ -0,0 +1,13 @@
+package Gittest1;
+
+public class for_learn_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abc");
+		System.out.println("123");
+		System.out.println("xyz");
+
+	}
+
+}
